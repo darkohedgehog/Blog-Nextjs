25b9224d11a87eb4e64f4b3bf99aafe532d0ebf6
darkohedgehog <zivic.darko79@gmail.com>
2023-09-26 22:08:19 +0200

    Modified files

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,11 @@
-import './globals.css'
-import { Inter, Manrope } from 'next/font/google'
-import { cx } from './utils'
+import './globals.css';
+import { Inter, Manrope } from 'next/font/google';
+import Header from '../components/Header';
+import { cx } from '../utils';
 
-const inter = Inter({ subsets: ['latin'], display: "swap", variable: "--font-in" })
+const inter = Inter({ subsets: ['latin'], display: "swap", variable: "--font-in" });
 
-const manrope = Manrope({ subsets: ['latin'], display: "swap", variable: "--font-mr" })
+const manrope = Manrope({ subsets: ['latin'], display: "swap", variable: "--font-mr" });
 
 export const metadata = {
   title: 'Create Next App',
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
         inter.variable,
         manrope.variable, 
         "font-mr bg-light dark:bg-dark")}>
+          <Header />
           {children}
      </body>
     </html>
---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,22 @@
+// please update required information
+const siteMetadata = {
+    title: 'Next.js Blog With Tailwind CSS and Contentlayer',
+    author: 'CodeBucks',
+    headerTitle: 'Next.js Blog',
+    description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
+    language: 'en-us',
+    theme: 'system', // system, dark or light
+    siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
+    siteLogo: '/logo.png',
+    socialBanner: '/social-banner.png', // add social banner in the public folder
+    email: 'zivic.darko79@gmail.com', 
+    github: 'https://github.com/darkohedgehog',
+    twitter: 'https://twitter.com',
+    facebook: 'https://www.facebook.com/zivic.vu/',
+    youtube: 'https://youtube.com',
+    linkedin: 'https://www.linkedin.com/in/darko-%C5%BEivi%C4%87/',
+    instagram: 'https://www.instagram.com',
+    locale: 'en-US',
+  }
+  
+  module.exports = siteMetadata;
